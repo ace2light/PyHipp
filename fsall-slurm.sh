dbf99aec8042d608c82dfa3a5d626aafbbd2e6bd
AWS ParallelCluster user <ec2-user@ip-10-0-0-128.ap-southeast-1.compute.internal>
2023-11-04 03:45:19 +0000

    add 2 new files consol_fs.jobs.sh and fsall-slurm.sh

---
+++ fsall-slurm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+<<<<<<< HEAD
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=1      # number of processors per task
+=======
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+>>>>>>> upstream/main
+#SBATCH -J "rpllfp"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o rpllfp-slurm.%N.%j.out # STDOUT
+#SBATCH -e rpllfp-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+<<<<<<< HEAD
+import DataProcessingTools as DPT; \
+lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
+lfall.save(); \
+hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
+hfall.save();
+>>>>>>> upstream/main
+
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:540128592471:awsnotify --message "FSJobDone"
